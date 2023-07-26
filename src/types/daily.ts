a5cb9d32d3c200dc0055aa734926d9591c25343e
JuanG <juan.gsanchez13@gmail.com>
2023-07-26 13:13:38 +0200

    Create app environment

---
+++ src/types/daily.ts
@@ -0,0 +1,6 @@
+export type Daily = {
+  time: string[];
+  weathercode: number[];
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+};
---
+++ src/types/daily.units.ts
@@ -0,0 +1,5 @@
+export type DailyUnits = {
+  time: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+};
